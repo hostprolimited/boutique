efa1e203c8c713809f17c8e45552c03b0e21d16e
hostproltd <info@hostpro.co.ke>
2022-07-04 09:21:14 +0300

    Initial Commit

--- lib/login.dart
+++ lib/login.dart
@@ -13,87 +13,90 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController txtemail = TextEditingController();
   TextEditingController txtpassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       body: Container(
         padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-                height: 250,
-                width: 250,
-                child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/logob.jpg'))),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-                controller: txtemail,
-                decoration: InputDecoration(
-                  labelText: 'email',
-                  hintText: 'email',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                ),
-                validator: (Value) {
-                  if (Value!.isEmpty) {
-                    return "Enter an email";
-                  } else {
-                    return null;
-                  }
-                }),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-                controller: txtpassword,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  hintText: 'password',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                ),
-                validator: (Value) {
-                  if (Value!.isEmpty) {
-                    return "Enter a Password";
-                  } else {
-                    return null;
-                  }
-                }),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DashboardPAge(),
-                          ));
-                    },
-                    child: Text('Login')),
-                SizedBox(
-                  width: 30,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterPage(),
-                          ));
-                    },
-                    child: Text('Register')),
-              ],
-            )
-          ],
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 250,
+                  width: 250,
+                  child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/logob.jpg'))),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                  controller: txtemail,
+                  decoration: InputDecoration(
+                    labelText: 'email',
+                    hintText: 'email',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                  ),
+                  validator: (Value) {
+                    if (Value!.isEmpty) {
+                      return "Enter an email";
+                    } else {
+                      return null;
+                    }
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                  controller: txtpassword,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'password',
+                    hintText: 'password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                  ),
+                  validator: (Value) {
+                    if (Value!.isEmpty) {
+                      return "Enter a Password";
+                    } else {
+                      return null;
+                    }
+                  }),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DashboardPAge(),
+                            ));
+                      },
+                      child: Text('Login')),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterPage(),
+                            ));
+                      },
+                      child: Text('Register')),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
